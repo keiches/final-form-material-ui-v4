7c7457891ce5dc1bb27b524582cc62b4be533f59
Brian Kim <keiches@gmail.com>
2020-01-03 20:59:44 +0900

    * Version up to Material-UI 4.8.2. * Redesign   - Select, TextField

--- src/Select.tsx
+++ src/Select.tsx
@@ -1,41 +1,114 @@
 /* eslint-disable react/prop-types, react/jsx-filename-extension, flowtype/object-type-delimiter, flowtype/delimiter-dangle */
 
 import * as React from 'react';
-import Select from '@material-ui/core/Select';
-import FormControl from '@material-ui/core/FormControl';
+import {Field, FieldProps, FieldRenderProps} from 'react-final-form';
+import MuiSelect, {SelectProps as MuiSelectProps} from '@material-ui/core/Select';
+import FormControl, {FormControlProps} from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
 import FormHelperText from '@material-ui/core/FormHelperText';
-import {FormControlProps} from '@material-ui/core/FormControl';
-import {FieldRenderProps} from 'react-final-form';
 
-// eslint-disable-next-line flowtype/no-weak-types
-interface SelectWrapperProps extends FieldRenderProps<any, HTMLElement> {
+export type SelectProps = MuiSelectProps & {
+  label?: string;
+  formControlProps?: FormControlProps;
+  fieldProps?: FieldProps<any, any>;
+};
+/*
+interface SelectProps extends FieldRenderProps<MuiSelectProps, HTMLSelectElement> {
   label: string,
   formControlProps: FormControlProps,
 }
+*/
+
+// eslint-disable-next-line flowtype/no-weak-types
+type SelectWrapperProps = FieldRenderProps<SelectProps, HTMLSelectElement>;
 
 const SelectWrapper: React.ComponentType<SelectWrapperProps> = ({
                                                                   input: {
-                                                                    name, value, onChange, ...restInput
-                                                                  }, meta, label, formControlProps, ...rest
+                                                                    name,
+                                                                    value,
+                                                                    onChange,
+                                                                    ...restInput
+                                                                  },
+                                                                  meta,
+                                                                  children,
+                                                                  ...restProps
                                                                 }) => {
-  const showError = ((meta.submitError && !meta.dirtySinceLastSubmit) || meta.error) && meta.touched;
+  const {labelWidth} = restProps as any;
 
   return (
-    <FormControl {...formControlProps} error={showError}>
-      <InputLabel htmlFor={name}>{label}</InputLabel>
+    <MuiSelect
+      labelWidth={labelWidth}
+      // InputLabelProps={{shrink: !!value}}
+      inputProps={restInput as any}
+      {...restProps}
+    >
+      {children}
+    </MuiSelect>
+  );
+};
 
-      <Select
-        {...rest}
-        name={name}
-        onChange={onChange}
-        inputProps={restInput}
-        value={value}
+const Select: React.ComponentType<SelectProps> = ({
+                                                    name,
+                                                    label,
+                                                    formControlProps,
+                                                    fieldProps,
+                                                    ...restProps
+                                                  }) => {
+  const {meta} = fieldProps as any;
+  const {labelWidth: labelWidthFixed} = restProps as any;
+  const showError = ((meta.submitError && !meta.dirtySinceLastSubmit) || meta.error) && meta.touched;
+
+  if (Number.isNaN(labelWidthFixed) || !labelWidthFixed) {
+    const refInputLabel = React.useRef(null);
+    const [labelWidth, setLabelWidth] = React.useState(0);
+    React.useEffect(() => {
+      // @ts-ignore
+      setLabelWidth(refInputLabel.current.offsetWidth);
+    }, []);
+
+    return (
+      <FormControl error={showError} {...formControlProps}>
+        {label && <InputLabel ref={refInputLabel} htmlFor={name}>{label}</InputLabel>}
+        <Field
+          name={name as any}
+          {...fieldProps}
+          render={({
+                     input,
+                     meta
+                   }) => (
+            <SelectWrapper
+              input={input as any}
+              meta={meta}
+              labelWidth={labelWidth}
+              {...restProps}
+            />
+          )}
+        />
+        {showError && <FormHelperText>{meta.error || meta.submitError}</FormHelperText>}
+      </FormControl>
+    );
+  }
+  return (
+    <FormControl error={showError} {...formControlProps}>
+      {label && <InputLabel htmlFor={name}>{label}</InputLabel>}
+      <Field
+        name={name as any}
+        {...fieldProps}
+        render={({
+                   input,
+                   meta
+                 }) => (
+          <SelectWrapper
+            input={input as any}
+            meta={meta}
+            labelWidth={labelWidthFixed}
+            {...restProps}
+          />
+        )}
       />
-
       {showError && <FormHelperText>{meta.error || meta.submitError}</FormHelperText>}
     </FormControl>
   );
 };
 
-export default SelectWrapper;
+export default Select;
--- src/TextField.tsx
+++ src/TextField.tsx
@@ -1,30 +1,63 @@
 /* eslint-disable react/prop-types, react/jsx-filename-extension */
 
 import * as React from 'react';
-import {FieldRenderProps} from 'react-final-form';
-import TextField from '@material-ui/core/TextField';
+import {Field, FieldProps, FieldRenderProps} from 'react-final-form';
+import MuiTextField, {TextFieldProps as MuiTextFieldProps} from '@material-ui/core/TextField';
 
-type TextFieldProps = FieldRenderProps<any,
-  HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>;
+const INPUT_TEXT = 'text';
+const INPUT_PASSWORD = 'password';
 
-const TextFieldWrapper: React.ComponentType<TextFieldProps> = ({
-                                                                 input: {
-                                                                   name, onChange, value, ...restInput
-                                                                 }, meta, ...rest
-                                                               }) => {
-  const showError = ((meta.submitError && !meta.dirtySinceLastSubmit) || meta.error) && meta.touched;
+export type TextFieldProps = Omit<MuiTextFieldProps, 'type'> & {
+  type: typeof INPUT_TEXT | typeof INPUT_PASSWORD;
+  fieldProps?: FieldProps<any, any>;
+};
+
+export type TextFieldWrapperProps = FieldRenderProps<TextFieldProps, HTMLInputElement | HTMLTextAreaElement>;
+
+const TextFieldWrapper: React.ComponentType<TextFieldWrapperProps> = ({
+                                                                        input: {
+                                                                          name,
+                                                                          value,
+                                                                          type = INPUT_TEXT,
+                                                                          onChange,
+                                                                          ...restInput
+                                                                        },
+                                                                        meta,
+                                                                        ...restProps
+                                                                      }) => {
+  const {helperText, error} = restProps as any;
+  const showError = error || (((meta.submitError && !meta.dirtySinceLastSubmit) || meta.error) && meta.touched);
 
   return (
-    <TextField
-      {...rest}
-      name={name}
-      helperText={showError ? meta.error || meta.submitError : undefined}
+    <MuiTextField
+      helperText={helperText || (showError ? meta.error || meta.submitError : undefined)}
       error={showError}
-      inputProps={restInput}
-      onChange={onChange}
       value={value}
+      type={type}
+      // InputLabelProps={{shrink: !!value}}
+      inputProps={restInput as any}
+      onChange={onChange}
+      {...restProps}
     />
   );
 };
 
-export default TextFieldWrapper;
+// const TextField: React.ComponentType<Partial<TextFieldProps>> = ({
+const TextField: React.ComponentType<TextFieldProps> = ({
+                                                          name,
+                                                          fieldProps,
+                                                          ...restProps
+                                                        }) => {
+  return (
+    <Field
+      name={name as any}
+      {...fieldProps}
+      render={({
+                 input,
+                 meta
+               }) => <TextFieldWrapper input={input} meta={meta} {...restProps}  />}
+    />
+  );
+};
+
+export default TextField;
